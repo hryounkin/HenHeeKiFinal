b4f84e359f2a3b062df37dacde1a5f83a0221d0e
hryounkin <u1511014@utah.edu>
2025-05-24 17:16:25 +0900

    Setup

--- final_project/data/TSX FILES/desert-ruins-preview.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="desert-ruins-preview" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../../../../final-project-2025-heheeki/resources/map/img/desert-ruins-preview.png" width="1024" height="1024"/>
-</tileset>
--- final_project/data/TSX FILES/terrain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="terrain" tilewidth="32" tileheight="32" tilecount="483" columns="21">
- <image source="../terrain.png" width="672" height="736"/>
-</tileset>
